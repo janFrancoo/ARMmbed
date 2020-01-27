81ccd7d8c3ffa1ce1565c61f9c84acab23c3855e
Eren Sırakaya <45636602+janFrancoo@users.noreply.github.com>
2020-01-28 02:14:41 +0300

    Create main.cpp

---
+++ CEN425 Labs/Lab - 5/main.cpp
@@ -0,0 +1,10 @@
+#include "mbed.h"
+
+PwmOut pwm8(PC_8);  //Create a PWM output called pwm8 on pin PC8
+
+int main()
+{
+    pwm8.period_ms(5);         //set PWM period to 10 ms
+    pwm8 = 0.2;                 //set duty cycle to %20
+    while(1);
+}
